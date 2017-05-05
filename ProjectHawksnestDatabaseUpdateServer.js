dac8382b4d433ba84ca8c2858547e0bdf786d22c
S. Nicknam <s.nicknam@icoud.com>
2017-05-05 05:48:26 +0200

    Automated randomized database

--- ProjectHawksnestDatabaseUpdateServer.js
+++ ProjectHawksnestDatabaseUpdateServer.js
@@ -12,12 +12,28 @@ admin.initializeApp({
 
 var db = admin.database();
 var attractionsRef = db.ref("attractions");
+var refreshRate;
+var seconds = (function(){var date = new Date(); return ((date.getMinutes()*60)+date.getSeconds());});;
+
 attractionsRef.once("value", function(snapshot) {
   console.log(snapshot.val());
 });
 
-attractionsRef.update({
-	"droomvlucht/open" : true,
-	"droomvlucht/waitingTime" : 15,
-	"sprookjesbos/waitingTime" : 15
+db.ref("refreshRate").on("value", function(snapshot) {
+	refreshRate = snapshot.val();
 });
+
+update();
+
+function update() {
+	if(seconds() % refreshRate == 0) {
+		attractionsRef.update({
+			"python/waitingTime" : 120*Math.random()
+});
+		setTimeout(update, (refreshRate - (seconds()%refreshRate) - 1) * 1000);
+	} else if(seconds <= 1) {
+		setTimeout(update, 10);
+	} else {
+		setTimeout(update, (refreshRate - (seconds()%refreshRate) - 1) * 1000);
+	}
+}
